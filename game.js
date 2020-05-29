6e6b49ddd696853db25b22c355692076069afbe0
Christy Turner Smith <CTurnerSmith1@humana.com>
2020-05-29 17:58:35 -0400

    It works

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-console.log("testing");
 let GreenButton = document.querySelector("#green");
 let YellowButton = document.querySelector("#yellow");
 let BlueButton = document.querySelector("#blue");
@@ -6,12 +5,20 @@ let RedButton = document.querySelector("#red");
 const playButton = document.querySelector("#Play");
 const resetButton = document.querySelector("#Replay");
 
+const randomColors = function () {
+    const colorAllbuttons = ["green", "red", "yellow", "blue"];
+    colorAllbuttons.sort(function() {
+        return .5-Math.random();
+    })
+return colorAllbuttons;
+};
+const randomButtons = randomColors();
 
 
 let guesses = [];
 let clicks = 0;
 GreenButton.addEventListener("click", function(event) {
-    guesses.push("Green");
+    guesses.push("green");
     GreenButton.style.backgroundColor = "white";
     clicks = clicks+1
     if (clicks === 4) {
@@ -20,7 +27,7 @@ GreenButton.addEventListener("click", function(event) {
 })
 
 YellowButton.addEventListener("click", function(event) {
-    guesses.push("Yellow");
+    guesses.push("yellow");
    YellowButton.style.backgroundColor = "white";
     clicks = clicks+1
     if (clicks === 4) {
@@ -29,7 +36,7 @@ YellowButton.addEventListener("click", function(event) {
 })
 
 BlueButton.addEventListener("click", function(event) {
-    guesses.push("Blue"); 
+    guesses.push("blue"); 
     BlueButton.style.backgroundColor = "white";
     clicks = clicks+1
     if (clicks === 4) {
@@ -38,46 +45,75 @@ BlueButton.addEventListener("click", function(event) {
 })
 
 RedButton.addEventListener("click", function(event) {
-    guesses.push("Red");
+    guesses.push("red");
     RedButton.style.backgroundColor = "white";
     clicks = clicks+1
     if (clicks === 4) {
         finishgame();
     }
 })
-console.log('one');
+
 function finishgame() {
-    if (guesses[0] === "Green" && guesses[1] === "Yellow" && guesses [2] === "Red" && guesses[3] === "Blue" && guesses){
-        alert ("Simon Says You Won!")
+    let Won = true
+  for (let i = 0; i < randomButtons.length;i++) {
+      console.log (randomButtons[i]);
+      console.log (guesses[i]);
+      if (randomButtons[i] !== guesses[i]) {
+          Won = false 
+      }
     }
+  if (Won) {
+      alert("Simon Says You Won");
+  } else {
+      alert ("Simon Says You Lost");
+  }
 };
 
-console.log("two");
+
 async function game() {
-    GreenButton.style.backgroundColor = "white";
-        setTimeout (function () {
-        GreenButton.style.backgroundColor = "green";
-        },1000)
-    await new Promise(resolve => setTimeout (resolve, 1000)) 
+    console.log(randomButtons);
+    for (let i=0; i < randomButtons.length; i++){
+        if (randomButtons[i] === "green"){
+            GreenButton.style.backgroundColor = "white";
+            setTimeout (function () {
+            GreenButton.style.backgroundColor = "green";
+            },1000)
+            await new Promise(resolve => setTimeout (resolve, 1000))
+        }else if (randomButtons[i] === "yellow"){
+            YellowButton.style.backgroundColor = "white";
+            setTimeout (function () {
+            YellowButton.style.backgroundColor = "yellow";
+            },1000)
+        await new Promise(resolve => setTimeout (resolve, 1000)) 
+        }else if (randomButtons[i] ==="red"){
+            RedButton.style.backgroundColor = "white";
+            setTimeout (function () {
+            RedButton.style.backgroundColor = "red";
+            },1000)
+            await new Promise(resolve => setTimeout (resolve, 1000))  
+        }else if (randomButtons[i] === "blue"){
+            BlueButton.style.backgroundColor = "white";
+            setTimeout (function () {
+            BlueButton.style.backgroundColor = "blue";
+            },1000)
+            await new Promise(resolve => setTimeout (resolve, 1000))
 
-    YellowButton.style.backgroundColor = "white";
-        setTimeout (function () {
-        YellowButton.style.backgroundColor = "yellow";
-        },1000)
-    await new Promise(resolve => setTimeout (resolve, 1000)) 
+        }
 
-    RedButton.style.backgroundColor = "white";
-        setTimeout (function () {
-        RedButton.style.backgroundColor = "red";
-        },1000)
-    await new Promise(resolve => setTimeout (resolve, 1000))
 
-    BlueButton.style.backgroundColor = "white";
-        setTimeout (function () {
-        BlueButton.style.backgroundColor = "blue";
-        },1000)
-    await new Promise(resolve => setTimeout (resolve, 1000))
+    }
+function playGreen(GreenButton) {
+    let audio = document.getElementById("1stclip");
+    audio.play();
 }
+
+
+
+
+  }
+    
+
+
 
 console.log(playButton);
 playButton.addEventListener('click', game);
