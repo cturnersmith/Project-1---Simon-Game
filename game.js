be208c85d9a9956fceb6596a50a454fbc1a46a85
Christy Turner Smith <CTurnerSmith1@humana.com>
2020-05-29 11:36:38 -0400

    save

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ let YellowButton = document.querySelector("#yellow");
 let BlueButton = document.querySelector("#blue");
 let RedButton = document.querySelector("#red");
 const playButton = document.querySelector("#Play");
+const resetButton = document.querySelector("#Replay");
 
 
 
@@ -45,9 +46,8 @@ RedButton.addEventListener("click", function(event) {
 })
 function finishgame() {
     if (guesses[0] === "Green"&& guesses[1] === "Yellow" && guesses [2] === "Red" && guesses[3] === "Blue" && guesses){
-        alert ("You Won!")
-    }
-}
+        alert ("Simon Says You Won!")
+};
 
 
 async function game() {
@@ -71,74 +71,16 @@ BlueButton.style.backgroundColor = "white";
     setTimeout (function () {
      BlueButton.style.backgroundColor = "blue";
     },1000)
-await new Promise(resolve => setTimeout (resolve, 1000));
+await new Promise(resolve => setTimeout (resolve, 1000))
 
 }
 
 // game()
 
 playButton.addEventListener('click', game);
+resetButton.addEventListener('click', ()=> {
+    location.reload();
 
 
-
-
-
-
-// for (let i = 0; i < 4; i++) {
-//     GreenButton.style.backgroundColor = "white";
-//     setTimeout (function () {
-//         GreenButton.style.backgroundColor = "green";
-//     },1000)
-// }
-
-
-
-
-
-
-
-
-
-
-
-// let game = {
-//     count: 0,
-//     colors: ["#green", "#red", "#yellow", "#blue"],
-//     compSeq: [],
-//     playerSeq: [],
-//     sound: {
-//       green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
-//       red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
-//       blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
-//       yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
-    
-// };
-
-// const Green = document.querySelector ("#green");
-// const Red = document.querySelector ("#red");
-// const Blue = document.querySelector ("#blue");
-// const Yellow = document.querySelector ("#yellow");
-
-// function sound (color) {
-//     switch (color) {
-//       case "#green":
-//         game.sound.green.play();
-//         break;
-//       case "#red":
-//         game.sound.red.play();
-//         break;
-//       case "#yellow":
-//         game.sound.yellow.play();
-//         break;
-//       case "#blue":
-//         game.sound.blue.play();
-//         break;
-//     }
-
-//   function animation(color) {
-//     $(color).addClass("animate");
-//     sound(color);
-//     setTimeout(function() {
-//       $(color).removeClass("animate");
-//     }, 500)
-//   }
+})
+}
